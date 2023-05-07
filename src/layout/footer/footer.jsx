efbccbd5b7e41b8b733b84b699dba1cffd03f8b7
Feterson <feterson@bk.ru>
2023-05-08 01:56:00 +0600

    create header/footer/main

---
+++ src/layout/footer/footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+
+    return(
+        
+        <footer className="page-footer  purple accent-4">
+       
+          <div className="footer-copyright">
+            <div className="container">
+            © {new Date().getFullYear()} Copyright Text
+            <a className="grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+          </div>
+        </footer>
+    )
+
+}
+
+export default Footer;
---
+++ src/layout/main/main.jsx
@@ -0,0 +1,14 @@
+
+
+const Main = () => {
+    return (
+        <main className="container content">
+            hello
+        </main>
+
+    )
+
+    
+}
+
+export default Main;
